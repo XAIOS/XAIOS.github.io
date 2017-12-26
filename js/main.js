378d2118cc74c8c0896b64ec41f9a6a0babc4514
XAIOS <1070053575@qq.com>
2017-12-26 22:44:37 +0800

    Site updated: 2017-12-26 22:44:36

--- js/main.js
+++ js/main.js
@@ -1,71 +1,61 @@
-  var _menuOn = false;
+_menuOn = false
 
-  function _menuShow() {
-    $('nav a').addClass('menu-active');
-    $('.menu-bg').show();
-    $('.menu-item').css({
-      opacity: 0
-    });
-    TweenLite.to('.menu-container', 1, {
-      padding: '0 40px'
-    });
-    TweenLite.to('.menu-bg', 1, {
-      opacity: '0.92'
-    });
-    TweenMax.staggerTo('.menu-item', 0.5, {
-      opacity: 1
-    }, 0.3);
-    _menuOn = true;
+function _menuShow() {
+  $('nav a').addClass('menu-active')
+  $('.menu-bg').show()
+  $('.menu-item').css({
+    opacity: 0
+  })
+  TweenLite.to('.menu-container', 1, {
+    padding: '0 40px'
+  })
+  TweenLite.to('.menu-bg', 1, {
+    opacity: '.92'
+  })
+  TweenMax.staggerTo('.menu-item', .5, {
+    opacity: 1
+  }, .3)
+  _menuOn = true
 
-    $('.menu-bg').hover(function() {
-      $('nav a').toggleClass('menu-close-hover');
-    });
-  }
+  $('.menu-bg').hover(function() {
+    $('nav a').toggleClass('menu-close-hover')
+  })
+}
 
-  function _menuHide() {
-    $('nav a').removeClass('menu-active');
-    TweenLite.to('.menu-bg', 0.5, {
-      opacity: '0',
-      onComplete: function() {
-        $('.menu-bg').hide();
-      }
-    });
-    TweenLite.to('.menu-container', 0.5, {
-      padding: '0 100px'
-    });
-    $('.menu-item').css({
-      opacity: 0
-    });
-    _menuOn = false;
-  }
+function _menuHide() {
+  $('nav a').removeClass('menu-active');
+  TweenLite.to('.menu-bg', .5, {
+    opacity: 0,
+    onComplete: function() {
+      $('.menu-bg').hide()
+    }
+  })
+  TweenLite.to('.menu-container', .5, {
+    padding: '0 100px'
+  })
+  $('.menu-item').css({
+    opacity: 0
+  })
+  _menuOn = false
+}
 
-  function initMenu() {
-    $('nav a').click(function() {
-      if (_menuOn) {
-        _menuHide();
-      } else {
-        _menuShow();
-      }
-    });
+$('nav a').click(function() {
+  _menuOn ? _menuHide() : _menuShow()
+})
 
-    $('.menu-bg').click(function(e) {
-      if (_menuOn && e.target === this) {
-        _menuHide();
-      }
-    });
-  }
+$('.menu-bg').click(function(e) {
+  if (_menuOn && e.target === this)
+    _menuHide()
+})
 
-  function displayArchives() {
-    $('.archive-post').css({
-      opacity: 0
-    });
-    TweenMax.staggerTo('.archive-post', 0.4, {
-      opacity: 1
-    }, 0.15);
-  }
+$('.archive-post').css({
+  opacity: 0
+})
 
-initMenu();
-displayArchives();
+TweenMax.staggerTo('.archive-post', .4, {
+  opacity: 1
+}, .15)
+
 $('.menu-bg').on('touchmove', function(e) {
   e.preventDefault()
 })
